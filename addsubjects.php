c238207e301a892edccf470c102b36201c0387cb
FloBurke <burke.fwn@oundleschool.org.uk>
2023-11-08 10:15:58 +0000

    adjustments

--- addsubjects.php
+++ addsubjects.php
@@ -9,8 +9,8 @@ try{
    
     $stmt = $conn->prepare("INSERT INTO TblSubjects (SubjectID,Subjectname,Teacher)VALUES (NULL,:subjectname,:teacher)");
 
-    $stmt->bindParam(':Subjectname', $_POST["subjectname"]);
-    $stmt->bindParam(':Teacher', $_POST["teacher"]);
+    $stmt->bindParam(':subjectname', $_POST["subjectname"]);
+    $stmt->bindParam(':teacher', $_POST["teacher"]);
     $stmt->execute();
 }
 catch(PDOException $e)
