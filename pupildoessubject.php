686c355dbd26544fc4c8477f68a2a7eb527058f9
FloBurke <burke.fwn@oundleschool.org.uk>
2023-11-17 09:10:43 +0000

    pupildoessubjects works

--- pupildoessubject.php
+++ pupildoessubject.php
@@ -1,1 +1,33 @@
-SELECT * FROM TblUsers WHERE Role = 0 ORDER BY Surname ASC;
+<!DOCTYPE html>
+<html>
+<head>
+    
+    <title>Page title</title>
+    
+</head>
+<body>
+<select name = "student">
+<?php
+include_once('connection.php');
+$stmt = $conn->prepare("SELECT * FROM TblUsers WHERE Role = 0 ORDER BY Surname ASC");
+$stmt->execute();
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+	echo('<option value='.$row["UserID"].'>'.$row["Surname"].', '.$row["Forename"].'</option>');
+}
+?>
+</select>
+
+<select name = "subjects">
+<?php
+include_once('connection.php');
+$stmt = $conn->prepare("SELECT * FROM TblSubjects ORDER BY Subjectname ASC");
+$stmt->execute();
+while ($row = $stmt->fetch(PDO::FETCH_ASSOC))
+{
+	echo('<option value='.$row["SubjectID"].'>'.$row["Subjectname"].'</option>');
+}
+?>
+</select>
+</body>
+</html>
